9052a89187ba58a0c512029609e4ca94d9e9f16c
JehyunRa <jra4862@gmail.com>
2020-01-06 19:17:09 +0000

    adding assertEqual.js

---
+++ lotide/assertEqual.js
@@ -0,0 +1,14 @@
+// Function Implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😀Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 1);
+assertEqual(1, 2);
