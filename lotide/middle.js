46a6ce78e679b07de261941c8584e447b150b05e
JehyunRa <jra4862@gmail.com>
2020-01-07 19:51:16 +0000

    adding middle.js

---
+++ lotide/middle.js
@@ -0,0 +1,42 @@
+// Function Implementation
+// Test/Assertion Functions
+const eqArrays = function(arrA, arrB) {
+  if (arrA.length !== arrB.length) return false;
+
+  let i;
+  for (i = 0; i < arrA.length; i++) {
+    if (arrA[i] !== arrB[i]) return false;
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(arrA, arrB) {
+  let a = arrA;
+  let b = arrB;
+  if (eqArrays(a, b) === true) {
+    console.log('😀Assertion Passed: ', a, ' === ', b);
+  } else {
+    console.log('😀Assertion Failed: ', a, '!== ', b);
+  }
+};
+
+//Actual Function
+const middle = function(arr) {
+  let rtArr = [];
+  if (arr.length < 3) return rtArr;
+  if (arr.length % 2 === 0) {
+    rtArr.push(arr[arr.length / 2 - 1], arr[arr.length / 2]);
+  } else {
+    rtArr.push(arr[(arr.length - 1) / 2]);
+  }
+  return rtArr;
+};
+
+// Test Code
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
