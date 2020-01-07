0f2c8ed66e3a13a0684f3b771c497424f19384cd
JehyunRa <jra4862@gmail.com>
2020-01-07 18:43:39 +0000

    adding eqArrays.js

---
+++ lotide/eqArrays.js
@@ -0,0 +1,25 @@
+// Function Implementation
+const eqArrays = function(arrA, arrB) {
+  if (arrA.length !== arrB.length) return false;
+
+  let i;
+  for (i = 0; i < arrA.length; i++) {
+    if (arrA[i] !== arrB[i]) return false;
+  }
+
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😀Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Test Code
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
