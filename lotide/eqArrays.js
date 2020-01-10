100e7f6efe3c213a4998e10991621dd02faed36d
JehyunRa <jra4862@gmail.com>
2020-01-10 13:48:10 +0000

    adding nested array comparison to eqArrays.js

--- lotide/eqArrays.js
+++ lotide/eqArrays.js
@@ -4,7 +4,13 @@ const eqArrays = function(arrA, arrB) {
 
   let i;
   for (i = 0; i < arrA.length; i++) {
-    if (arrA[i] !== arrB[i]) return false;
+    if (Array.isArray(arrA[i]) && Array.isArray(arrB[i])) {
+      if (!eqArrays(arrA[i], arrB[i])) {
+        return false;
+      };
+    } else if (arrA[i] !== arrB[i]) {
+      return false;
+    }
   }
 
   return true;
@@ -23,3 +29,8 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+console.log('---');
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+assertEqual(eqArrays([[2, 3], [4, 5]], [[2, 3], [4, 5]]), true);
