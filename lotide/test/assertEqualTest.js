56a97f1acc670008837f30c6c6bc5fd8e6db7f3b
JehyunRa <jra4862@gmail.com>
2020-01-13 18:00:13 +0000

    fixing2

--- lotide/test/assertEqualTest.js
+++
@@ -1,8 +0,0 @@
-//Imported Function
-const assertEqual = require('../assertEqual');
-
-//Test Code - assertEqual
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 1);
-assertEqual(1, 2);
